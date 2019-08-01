3d0e8d2a470056811214c0b177c69cb801df7be9
Dan Houck <dan.houck@batpox.com>
2019-08-01 10:58:23 -0400

    Add RunTime folder

---
+++ RunTime/(readme).txt
@@ -0,0 +1,2 @@
+You can move these files to someplace where Simio picks them up (e.g. your Documents > SimioUserExtensions)
+and then they will be added to your Simio model (as Add-In).
--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Simio, LLC")]
 [assembly: AssemblyProduct("GisAddIn")]
-[assembly: AssemblyCopyright("Copyright ©  2018 Simio LLC")]
+[assembly: AssemblyCopyright("Copyright 2018-2019 Simio LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.2.13.0")]
-[assembly: AssemblyFileVersion("18.2.13.0")]
+[assembly: AssemblyVersion("19.8.1.0")]
+[assembly: AssemblyFileVersion("19.8.1.0")]
